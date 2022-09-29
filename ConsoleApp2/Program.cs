3189625e20cd05be3ebcc56e43381a49b38c6ee8
Horta <Horta@DESKTOP-DQM7GVE>
2022-09-29 23:59:48 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,17 @@
+﻿//Миронова Анастасия 22-ИСП-2/1
+//Задание 1.2, уровень высокий, вариант 4
+try
+{
+    Console.Write("Введите катет:");
+    double cathet = double.Parse(Console.ReadLine());
+    Console.Write("Введите гипотинузу:");
+    double gipot = double.Parse(Console.ReadLine());
+    double S = cathet/2 * Math.Sqrt(gipot * gipot - cathet * cathet);
+    double cathetTwo = Math.Sqrt(gipot * gipot) - (cathet * cathet);
+    Console.WriteLine($"Площадь:{S:F2}");
+    Console.WriteLine($"Катет:{cathetTwo:F2}");
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.ToString());
+}
